a2c55ba7b98b26029558e44c01c54a1892f2012e
Gustavo <guhlavelli@gmail.com>
2022-08-15 21:38:00 -0300

    Changes screen: nublado.dart

--- lib/Screen/nublado.dart
+++ lib/Screen/nublado.dart
@@ -10,10 +10,10 @@ class Nublado extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: RadialGradient(
           center: Alignment(0, -0.1), // near the top right
-          radius: 0.5,
+          radius: 0.6,
           colors: <Color>[
-            Color.fromRGBO(120, 120, 155, 0.9), // yellow sun
-            Color.fromRGBO(144, 159, 167, 1), // blue sky
+            Color.fromRGBO(132, 171, 193, 0.9), // yellow sun
+            Color.fromRGBO(74, 116, 135, 1), // blue sky
           ],
           stops: <double>[0.1, 1.0],
         )
@@ -57,6 +57,35 @@ class Nublado extends StatelessWidget {
               child:  Text('21°', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),),
             ),
           ],
+        ),
+        bottomNavigationBar:
+        Container(
+            height:  MediaQuery.of(context).size.height * .1,
+            width: MediaQuery.of(context).size.width * .99,
+            decoration: const BoxDecoration(
+                color: Color.fromRGBO(173, 173, 173, 1),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(50.0),topRight: Radius.circular(50.0) )
+            ),
+            child: Row(
+                children: const <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text('Histórico', style: TextStyle(fontSize: 20, )),
+                  ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Center(
+                    child: Text('Today', style: TextStyle(fontSize: 20, ),),
+                  ),
+                  SizedBox(
+                    width: 70,
+                  ),
+                  Center(
+                    child: Text('Future', style: TextStyle(fontSize: 20, ),),
+                  )
+                ]
+            )
         ),
       ),
     );
